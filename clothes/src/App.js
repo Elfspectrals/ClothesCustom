b94d2805257cc5250e4ed2d2973558eccef12445
Jerome <jerome.neupert@epitech.eu>
2024-06-22 18:39:02 +0200

    Clothes 1

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import Model from './clothes1/Scene'; // Adjust the path as necessary
 import './App.css';
 
 const App = () => {
-  return <Model color='blue'/>;
+  const [color, setColor] = useState('blue'); // Initial color set to blue
+
+  return (
+    <div>
+      <input 
+        type="color" 
+        value={color} 
+        onChange={(e) => setColor(e.target.value)} // Update color based on input
+      />
+      <Model color={color}/>
+    </div>
+  );
 };
 
 export default App;
--- clothes/src/clothes1/Scene.jsx
+++ clothes/src/clothes1/Scene.jsx
@@ -7,11 +7,10 @@ export default function Model({ color = "red" }) { // Corrected line
 
   return (
     <Canvas camera={{ position: [0, 0, 20], fov: 75 }}>
-      <Environment preset="sunset" />
+      <Environment preset="studio" background/>
       <ambientLight intensity={1} />
       <Suspense fallback={null}>
         <group dispose={null} scale={10} position={[0, -5, 0]}>
-          {/* Corrected the material-color prop to use the three.js standard for setting material properties dynamically */}
           <mesh geometry={nodes.defaultMaterial.geometry} material={materials.Collar_FRONT_5309977} scale={0.01} material-color={color} />
         </group>
         <OrbitControls />
