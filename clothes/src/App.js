22fce3a372c33998113ae552dfd0a2f6cae0741c
Jerome <jerome.neupert@epitech.eu>
2024-06-22 10:54:02 +0200

    Props add for model

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,20 +1,9 @@
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
-import Model from './clothes1/Scene'
-import './App.css'
+import React from 'react';
+import Model from './clothes1/Scene'; // Adjust the path as necessary
+import './App.css';
 
 const App = () => {
-  return (
-    <Canvas camera={{ position: [0, 0, 20], fov: 75 }}>
-      <Environment preset="sunset" />
-      <ambientLight intensity={1}/>
-      <Suspense fallback={null}>
-        <Model position={[0, -5, 0]} />
-        <OrbitControls  />
-      </Suspense>
-    </Canvas>
-  )
-}
+  return <Model color='blue'/>;
+};
 
-export default App
+export default App;
